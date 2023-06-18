2fabcf5d36d1685b0b243f452a0b45776ec7ac97
Samuel <Sommosgaming@gmail.com>
2023-06-18 19:23:03 +0100

    refactored for readability

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ df = pd.read_csv('datasets/avocado.csv')
 # make a copy of the original dataframe where the type is equal to 'organic'
 df = df[df['type'] == 'organic']
 # sort the dataframe by the values of the column 'Date'
-df.sort_values(by='Date', ascending=True, inplace=True)
+df.sort_values(by = 'Date', ascending = True, inplace = True)
 # sort the dataframe by the column 'Date'
 df['Date'] = pd.to_datetime(df['Date'])
 
@@ -22,7 +22,7 @@ for region in df['region'].unique():
     # create a new dataframe with the rows where the region is equal to the current region
     region_df = df[df['region'] == region]
     # set the index of the dataframe to the column 'Date', inplace=True means that the dataframe is updated
-    region_df.set_index('Date', inplace=True)
+    region_df.set_index('Date', inplace = True)
     # sort the dataframe by the index
     region_df.sort_index(inplace=True)
     # create a new column in the dataframe with the name 'price25ma' and the value of the rolling mean
@@ -43,11 +43,11 @@ max_legend_entries = 54
 # create a list of legend labels limited to the specified number of entries, and split the labels on capital letters
 limited_legend_labels = [re.sub(r'(?<=\w)([A-Z])', r' \1', label) for label in legend_labels[:max_legend_entries]]
 # plot the graph dataframe, dropping any NaN values, and set the size of the graph
-graph_complete = graph_df.dropna().plot(figsize=(20,12), legend=True)
+graph_complete = graph_df.dropna().plot(figsize = (20,12), legend = True)
 # set the title of the graph
-graph_complete.set_title('Avocado Average Region Prices', size=30, fontfamily='Arial')
+graph_complete.set_title('Avocado Average Region Prices', size = 30, fontfamily = 'Arial')
 # set the x-axis label of the graph to anchor outside of the graph, on the right side with 1 column and a font size of 7
-graph_complete.legend(labels=limited_legend_labels, loc='center left', bbox_to_anchor=(1.0, 0.5), ncol=1, prop={'family': 'Arial', 'size': 7})
+graph_complete.legend(labels = limited_legend_labels, loc = 'center left', bbox_to_anchor = (1.0, 0.5), ncol = 1, prop = {'family': 'Arial', 'size': 7})
 
 # create the mplcursors cursor object for the plot
 cursors = mplcursors.cursor(graph_complete)
